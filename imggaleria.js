3453af90403810d4de5c28b30285127b3b35a541
dayronmento <dayronmento@misena.educ.o>
2015-08-04 17:16:36 -0500

    Galeria de imagenes

---
+++ imggaleria.js
@@ -0,0 +1,23 @@
+function cambio0(){
+	document.getElementById('imgprincipal').src='./imagenes/imagen0.jpg';
+}
+
+function cambio1(){
+	document.getElementById('imgprincipal').src='./imagenes/imagen1.jpg';
+}
+
+function cambio2(){
+	document.getElementById('imgprincipal').src='./imagenes/imagen2.jpg';
+}
+
+function cambio3(){
+	document.getElementById('imgprincipal').src='./imagenes/imagen3.jpg';
+}
+
+function cambio4(){
+	document.getElementById('imgprincipal').src='./imagenes/imagen4.jpg';
+}
+
+function volver(){
+	document.getElementById('imgprincipal').src='./imagenes/imagen.jpg';
+}
